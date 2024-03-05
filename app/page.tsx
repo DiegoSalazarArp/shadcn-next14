06fb8ce85a8e9c37ab9babfcc96b503e7698f26d
DiegoSalazar <d.salazararp@gmail.com>
2024-03-04 22:59:07 -0300

    se crea header y notificaciones layout

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <Button variant={"outline"}>Click me</Button>
+    <div>
+      Homepage
+      <Button variant={"outline"}>Click me</Button>
+    </div>
   );
 }
